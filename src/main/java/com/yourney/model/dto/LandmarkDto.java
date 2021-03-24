0027e51e77653d15ebb4970558ff824b9a30d1a4
edubotdom <55277082+edubotdom@users.noreply.github.com>
2021-03-24 01:17:34 +0100

    Corregido estilo y eliminadas dependencias innecesarias

--- src/main/java/com/yourney/model/dto/LandmarkDto.java
+++ src/main/java/com/yourney/model/dto/LandmarkDto.java
@@ -1,9 +1,7 @@
 package com.yourney.model.dto;
 
 import java.time.LocalDateTime;
-import java.util.List;
 
-import com.yourney.model.SeasonType;
 import com.yourney.model.StatusType;
 
 import lombok.AllArgsConstructor;
@@ -12,44 +10,44 @@ import lombok.Data;
 @Data
 @AllArgsConstructor
 public class LandmarkDto {
-    
+
     private long id;
 
     private String name;
-	  
+
     private String description;
-      
+
     private Double price;
-	
+
     private String country;
-      
+
     private String city;
-      
+
     private Double latitude;
-      
+
     private Double longitude;
-      
-    //private boolean promoted;
-      
+
+    // private boolean promoted;
+
     private String category;
 
-	private String email;
-	
-	private String phone;
-	
-	private String website;
-	
-	private String instagram;
-	
-	private String twitter;
+    private String email;
+
+    private String phone;
+
+    private String website;
+
+    private String instagram;
+
+    private String twitter;
 
     private LocalDateTime deleteDate;
 
     private LocalDateTime updateDate;
 
-    private LocalDateTime createDate;     
+    private LocalDateTime createDate;
 
     private Long views;
 
-    private StatusType	status;
+    private StatusType status;
 }
--- src/main/java/com/yourney/security/controller/AuthController.java
+++ src/main/java/com/yourney/security/controller/AuthController.java
@@ -17,7 +17,6 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
