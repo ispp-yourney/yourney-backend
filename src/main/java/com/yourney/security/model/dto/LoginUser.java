e8fda1d91a3a0a638a9cee5f1a86349f5f8902e5
javvazzam <javvazzam@alum.us.es>
2021-04-19 01:46:05 +0200

    Cambios en validaciones
    Se han validado campos que no lo estaban antes

--- src/main/java/com/yourney/security/model/dto/LoginUser.java
+++ src/main/java/com/yourney/security/model/dto/LoginUser.java
@@ -2,14 +2,18 @@ package com.yourney.security.model.dto;
 
 import javax.validation.constraints.NotBlank;
 
+import org.hibernate.validator.constraints.Length;
+
 import lombok.Data;
 
 @Data
 public class LoginUser {
 
+	@Length(max=50, message="No se permiten más de 50 caracteres")
 	@NotBlank(message = "El nombre de usuario es obligatorio")
 	private String username;
 
+	@Length(max=50, message="No se permiten más de 50 caracteres")
 	@NotBlank(message = "La contraseña es obligatoria")
 	private String password;
 
